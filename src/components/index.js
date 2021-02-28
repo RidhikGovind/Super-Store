22d092a50ce239e725cdb7b374866a50b17058ba
RidhikGovind <ridzman2019@gmail.com>
2021-02-28 12:18:44 +0530

    reformatted code structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,4 @@
-import Home from './Home'
-import Deals from './Deals'
-import Cart from './Cart'
-import Nav from './Nav'
-import SearchBar from './SearchBar'
+import Nav from './Nav';
+import SearchBar from './SearchBar';
 
-export {Home,Deals,Cart, Nav, SearchBar}
+export { Nav, SearchBar };
